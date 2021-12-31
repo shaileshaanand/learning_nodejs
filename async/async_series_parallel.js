5056fb3f33db4b838a03b257de11f8bbac49d1a6
Shailesh Aanand <anaandshailu@gmail.com>
2021-12-31 13:05:39 +0530

    change to different timeouts

--- async/async_series_parallel.js
+++ async/async_series_parallel.js
@@ -4,7 +4,7 @@ const get1 = (callback) => {
   setTimeout(() => {
     console.log(1);
     callback(null, 1);
-  }, 1000);
+  }, 1500);
 };
 const get2 = (callback) => {
   setTimeout(() => {
@@ -16,21 +16,21 @@ const get3 = (callback) => {
   setTimeout(() => {
     console.log(3);
     callback(null, 3);
-  }, 1000);
+  }, 400);
 };
 
 const get4 = (callback) => {
   setTimeout(() => {
     console.log(4);
     callback(null, 4);
-  }, 1000);
+  }, 600);
 };
 
 const get5 = (callback) => {
   setTimeout(() => {
     console.log(5);
     callback(null, 5);
-  }, 1000);
+  }, 200);
 };
 
 async.series([get1, get2, get3, get4, get5], (err, data) => {
